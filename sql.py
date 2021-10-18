a9e662dc7abad29022e0b5eaa92b53211a05d4f0
choge <2502247952@qq.com>
2021-10-18 20:18:14 +0800

    2.0

--- sql.py
+++ sql.py
@@ -1,6 +1,7 @@
 import pymysql
 
-class insert(list):
+def insert(tu):
+    valuse=tu
     db = pymysql.connect(host='www.chaogezuishuai.top',
                            user="root",
                            port=3306,
@@ -8,9 +9,70 @@ class insert(list):
                            password="NRAHbsqt941",
                            charset="utf8")
     cursor=db.cursor()
-    sql=''
+    sql="INSERT INTO student(id,name,sex,adress,phone,major) values('%s','%s','%s','%s','%s','%s')"%valuse
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
+    return 1
+
+def selects(id):
+    db = pymysql.connect(host='www.chaogezuishuai.top',
+                           user="root",
+                           port=3306,
+                           db="student",
+                           password="NRAHbsqt941",
+                           charset="utf8")
+    cursor=db.cursor()
+    sql="SELECT * FROM student WHERE id='%s';"%id
+    cursor.execute(sql)
+    result=list(cursor.fetchall()[0])
+    result.remove(id)
+    db.commit()
+    cursor.close()
+    db.close()
+    return result
+
+def updata(tu):
+    valuse=list(tu)[1:]
+    id=list(tu)[0]
+    valuse.append(id)
+    valuse=tuple(valuse)
+    print(valuse)
+    db = pymysql.connect(host='www.chaogezuishuai.top',
+                           user="root",
+                           port=3306,
+                           db="student",
+                           password="NRAHbsqt941",
+                           charset="utf8")
+    cursor=db.cursor()
+    sql="UPDATE student set name='%s',sex='%s',adress='%s',phone='%s',major='%s' where id='%s';"%valuse
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
+    return 1
+
+def delete(id):
+    db = pymysql.connect(host='www.chaogezuishuai.top',
+                         user="root",
+                         port=3306,
+                         db="student",
+                         password="NRAHbsqt941",
+                         charset="utf8")
+    cursor = db.cursor()
+    sql = "DELETE FROM student WHERE id='%s';" % id
+    print(sql)
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
+    return 1
+
+
 
 
 if __name__=="__main__":
-    tu=(45641, 'dwa', 'fes', 'hdhr', 'sefd', 'sefg')
-    insert(tu)
+    tu=('45641', 'wa', 'fes', 'hdhr', '486', 'sefg')
+    #insert(tu)
+    #updata(tu)
--- student_message.py
+++ student_message.py
@@ -1,7 +1,102 @@
 from PyQt5.QtWidgets import QApplication,QDialog,QFileDialog
 import sys
+from sql import *
 from init import *
 from insert import *
+from success import *
+from updata_message import *
+from select_message import *
+from delete_message import *
+
+#删除信息
+class delete_message(QDialog,Ui_del_Dialog):
+    def __init__(self):
+        super(delete_message, self).__init__()
+        self.setupUi(self)
+        self.exit.clicked.connect(self.close)
+        self.delete_message.clicked.connect(self.viewdata)
+        self.clear_data.clicked.connect(self.clear)
+
+    def viewdata(self):
+        id=self.id.text()
+        print(id)
+        # if delete(id)==1:
+        #     dialog=success()
+        #     dialog.exec()
+        #     self.clear()
+
+    def clear(self):
+        self.id.clear()
+
+#更新信息
+class updata_message(QDialog,Ui_updata_Dialog):
+    def __init__(self):
+        super(updata_message, self).__init__()
+        self.setupUi(self)
+        self.exit.clicked.connect(self.close)
+        self.clear_data.clicked.connect(self.clear)
+        self.ok.clicked.connect(self.updata_sql)
+
+    #导入数据
+    def updata_sql(self):
+        id=self.id.text()
+        name=self.name.text()
+        sex=self.sex.text()
+        adress=self.adress.text()
+        phone=self.phone.text()
+        major=self.major.text()
+        tu=(id,name,sex,adress,phone,major)
+        if updata(tu)==1:
+            dialog=success()
+            dialog.exec()
+            self.clear()
+
+    def clear(self):
+        self.id.clear()
+        self.name.clear()
+        self.sex.clear()
+        self.adress.clear()
+        self.phone.clear()
+        self.major.clear()
+
+
+
+
+
+#查询信息
+class select_message(QDialog,Ui_se_Dialog):
+    def __init__(self):
+        super(select_message, self).__init__()
+        self.setupUi(self)
+        self.clear_data.clicked.connect(self.clear)
+        self.exit.clicked.connect(self.close)
+        self.select.clicked.connect(self.viewdata)
+
+    def viewdata(self):
+        id=self.id_data.text()
+        xinxi=selects(id)
+        self.name.setText(xinxi[0])
+        self.sex.setText(xinxi[1])
+        self.adress.setText(xinxi[2])
+        self.phone.setText(xinxi[3])
+        self.major.setText(xinxi[4])
+
+    def clear(self):
+        self.name.clear()
+        self.sex.clear()
+        self.adress.clear()
+        self.phone.clear()
+        self.major.clear()
+
+#操作成功
+class success(QDialog,Ui_su_Dialog):
+    def __init__(self):
+        super(success, self).__init__()
+        self.setupUi(self)
+        self.exit.clicked.connect(self.close)
+
+
+
 #插入信息界面
 class insert_message(QDialog,Ui_insert_Dialog):
     def __init__(self):
@@ -9,17 +104,28 @@ class insert_message(QDialog,Ui_insert_Dialog):
         self.setupUi(self)
         self.exit.clicked.connect(self.close)
         self.ok.clicked.connect(self.insert_sql)
-
+        self.clear_data.clicked.connect(self.clear)
+    #导入数据
     def insert_sql(self):
-        id=int(self.id.text())
+        id=self.id.text()
         name=self.name.text()
         sex=self.sex.text()
         adress=self.adress.text()
         phone=self.phone.text()
         major=self.major.text()
         tu=(id,name,sex,adress,phone,major)
-
-
+        if insert(tu)==1:
+            dialog=success()
+            dialog.exec()
+            self.clear()
+    #清空数据
+    def clear(self):
+        self.id.clear()
+        self.name.clear()
+        self.sex.clear()
+        self.adress.clear()
+        self.phone.clear()
+        self.major.clear()#
 
 
 #初始化界面
@@ -29,9 +135,25 @@ class main_dialog(QDialog,Ui_main_Dialog):
         self.setupUi(self)
         self.exit.clicked.connect(self.close)
         self.insert_xinxi.clicked.connect(self.insert)
+        self.select_data.clicked.connect(self.select)
+        self.update_data.clicked.connect(self.updata)
+        self.delete_2.clicked.connect(self.delete)
+
 
     def insert(self):
         dialog=insert_message()
+        dialog.exec()
+
+    def select(self):
+        dialog=select_message()
+        dialog.exec()
+
+    def updata(self):
+        dialog=updata_message()
+        dialog.exec()
+
+    def delete(self):
+        dialog=delete_message()
         dialog.exec()
 
 
